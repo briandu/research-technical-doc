da025282d4d3dc1912532ff3d1ae030a2371ab1c
Brian Du <brian.bh.du@gmail.com>
2017-04-01 18:34:38 -0400

    File complete

--- ruby.rb
+++ ruby.rb
@@ -1,4 +1,4 @@
-# String
+# STRING
 
 "I love pizza".length #=> 12
 
@@ -9,7 +9,7 @@
 "spaghetti".start_with?("spag") #=> true
 
 
-# Array
+# ARRAY
 
 a = [ 1, 5, 16, 30, 2 ]
 a.first #=> 1
@@ -30,4 +30,32 @@ a.pop #=> 2
 a #=> [1, 5, 16, 30]
 
 
-#
+# Hash
+
+h = { "a" => 252, "b" => 134, "c" => 507}
+h.to_a #=> [["a", 252], ["b", 134], ["c", 507]]
+
+h = { "a" => 252, "b" => 134, "c" => 507}
+h.has_key?("b") #=> true
+
+h = { "a" => 252, "b" => 134, "c" => 507}
+h.has_value?(507) #=> true
+
+
+# TIME
+
+Time.new(2017) #=> 2017-01-01 00:00:00 -0500
+Time.now #=> 2017-04-01 18:21:40 -0400
+
+
+# FILE
+
+File.exist?("/Users/briandu/bitmake
+r/projects/assignments/week 2/research-technical-doc
+/ruby.rb")
+#=> true
+
+File.extname("/Users/briandu/bitmake
+r/projects/assignments/week 2/research-technical-doc
+/ruby.rb")
+#=> ".rb"
